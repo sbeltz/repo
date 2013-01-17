125e593fb3f2f289a7b13afd59e1eb7ad99cd4c5
sbeltz <shelley.beltz@gettyimages.com>
2013-01-17 15:51:56 -0700

    Completed Task D

--- depot/app/controllers/application_controller.rb
+++ depot/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+    def current_cart
+        # find the cart with the given id, return the cart
+        Cart.find(session[:cart_id])
+        # if it's not found, then create a new cart and set 
+        # it in the session and return the cart
+        rescue ActiveRecord::RecordNotFound
+        cart = Cart.create
+        session[:cart_id] = cart.id
+        cart
+    end
 end
--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: { message: 'must be unique, silly' }
@@ -8,4 +12,16 @@ class Product < ActiveRecord::Base
       message: 'must be a URL for GIF, JPG, or PNG image.'
   }
   validates :title, length: { :minimum => 10, :too_short => "is too short and must contain at least 10 characters" }
+    
+    private
+    # ensure that ther eare no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+        if line_items.empty?
+            true
+        else
+            errors.add(:base, 'Line items present')
+            false
+        end
+    end
+
 end
