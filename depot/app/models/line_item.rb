c2b8a18237ac6e895893080e6956925c7effcac4
sbeltz <shelley.beltz@gettyimages.com>
2013-01-21 16:52:35 -0700

    completed task g

--- depot/app/models/line_item.rb
+++ depot/app/models/line_item.rb
@@ -2,6 +2,7 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id, :product, :quantity, :price
   belongs_to :product
   belongs_to :cart
+  belongs_to :order
 
   def total_price
     price * quantity
